1d6feb6c22ddc6e3f2553284e41107f8be38ec96
zhangtao <14010023+zhang_taowhu@user.noreply.gitee.com>
2024-07-11 15:17:18 +0800

    update debug config

--- omg_llava/omg_llava/model/modules/projector/modeling_projector.py
+++ omg_llava/omg_llava/model/modules/projector/modeling_projector.py
@@ -10,7 +10,8 @@ from torch.nn import functional as F
 from .configuration_projector import ProjectorConfig_OMG_LLaVA
 
 class Naive_Proj(nn.Module):
-    def __init__(self, config, rm_prior_embedding=False, rm_query=False):
+    def __init__(self, config, rm_prior_embedding=False,
+                 rm_query=False):
         super().__init__()
         query_channels = config.query_channels
         self.query_channels = query_channels
@@ -19,6 +20,9 @@ class Naive_Proj(nn.Module):
             query_channels = query_channels[0]
         if isinstance(feat_channels, tuple):
             feat_channels = feat_channels[0]
+
+        add_cross_attn_layer = config.add_cross_attn_layer
+        self.add_cross_attn_layer = config.add_cross_attn_layer
 
         query_channels = query_channels * 2  # feat + embed
 
@@ -38,6 +42,20 @@ class Naive_Proj(nn.Module):
                     config.llm_hidden_size,
                     bias=config.bias))
         self.model = nn.Sequential(*modules)
+
+        if add_cross_attn_layer:
+            print("Using Cross Attention Layer at Projector !!!")
+            self.query_cross_attn = CrossAttentionLayer(
+                d_model=config.llm_hidden_size,
+                nhead=32,
+            )
+            self.query_ffn = FFNLayer(
+                d_model=config.llm_hidden_size,
+                dim_feedforward=4096,
+            )
+        else:
+            self.query_cross_attn = None
+            self.query_ffn = None
 
         modules = [
             nn.Linear(
@@ -59,8 +77,47 @@ class Naive_Proj(nn.Module):
         self.rm_prior_embedding = rm_prior_embedding
         self.rm_query = rm_query
 
+        visual_prompt_proj = config.visual_prompt_proj
+        self.visual_prompt_proj = visual_prompt_proj
+        if not visual_prompt_proj:
+            self.visual_prompt_query_proj = None
+            self.visual_prompt_query_model = None
+            self.visual_prompt_query_cross_attn = None
+            self.visual_prompt_query_ffn = None
+        else:
+            print("Initialized all Layers for Visual Prompt in Projector !!!")
+            self.visual_prompt_query_proj = nn.Linear(query_channels, feat_channels)
+            modules = [
+                nn.Linear(
+                    feat_channels,
+                    config.llm_hidden_size,
+                    bias=config.bias)
+            ]
+            for _ in range(1, config.depth):
+                modules.append(ACT2FN[config.hidden_act])
+                modules.append(
+                    nn.Linear(
+                        config.llm_hidden_size,
+                        config.llm_hidden_size,
+                        bias=config.bias))
+            self.visual_prompt_query_model = nn.Sequential(*modules)
+
+            if add_cross_attn_layer:
+                self.visual_prompt_query_cross_attn = CrossAttentionLayer(
+                    d_model=config.llm_hidden_size,
+                    nhead=32,
+                )
+                self.visual_prompt_query_ffn = FFNLayer(
+                    d_model=config.llm_hidden_size,
+                    dim_feedforward=4096,
+                )
+            else:
+                self.visual_prompt_query_cross_attn = None
+                self.visual_prompt_query_ffn = None
+
     def forward(self, x):
         clip_feature, query_feat, attention_mask = x
+        query_feat_copy = query_feat[0, :1]  # (1, 1, c)
         # clip feature (bs, hw, c + 2 * q_c)
         # query_feat (bs, q, c)
         # attention_mask (bs, q, hw)
@@ -91,12 +148,73 @@ class Naive_Proj(nn.Module):
             valid_queries_embeddings.append(layer_output[keep])
         self.valid_queries_embeddings = valid_queries_embeddings
 
+        self.last_clip_feature = clip_feature_out
+
         for clip_feat, layer_output, keep in zip(clip_feature_out, layer_outputs, valid_mask):
+            valid_layer_output = layer_output[keep]
+            if self.add_cross_attn_layer:
+                valid_layer_output = self.query_cross_attn(
+                    valid_layer_output.unsqueeze(1), clip_feat.unsqueeze(1),
+                )[:, 0]
+                valid_layer_output = self.query_ffn(valid_layer_output)
             if self.rm_query:
-                ret.append(clip_feat + torch.mean(self.seperate_embed.weight) * 0.0 + torch.mean(layer_output[keep]) * 0.0)
+                ret.append(clip_feat + torch.mean(self.seperate_embed.weight) * 0.0 + torch.mean(valid_layer_output) * 0.0)
             else:
-                ret.append(torch.cat([clip_feat, self.seperate_embed.weight, layer_output[keep]], dim=0))
+                ret.append(torch.cat([clip_feat, self.seperate_embed.weight, valid_layer_output], dim=0))
+
+        # generate zero using visual prompt projector if valid
+        if self.visual_prompt_proj:
+            visual_prompt_embeddings = query_feat_copy.to(self.visual_prompt_query_proj.weight.dtype)
+            visual_prompt_embeddings = self.visual_prompt_query_proj(visual_prompt_embeddings)
+            visual_prompt_embeddings = self.visual_prompt_query_model(visual_prompt_embeddings)  # (B, C)
+            if self.add_cross_attn_layer:
+                clip_feat = self.last_clip_feature[0]  # (B, HW, C)
+                visual_prompt_embeddings = self.visual_prompt_query_cross_attn(
+                    visual_prompt_embeddings.unsqueeze(1), clip_feat.unsqueeze(1),
+                )[:, 0]
+                visual_prompt_embeddings = self.visual_prompt_query_ffn(visual_prompt_embeddings)
+            self.visual_prompt_zero = visual_prompt_embeddings.sum() * 0.0
+        else:
+            self.visual_prompt_zero = 0.0
         return ret
+
+    def forward_visual_prompts_embeddings(self, visual_prompt_embeddings, batch_idxs):
+        if self.visual_prompt_proj:
+            visual_prompt_embeddings = visual_prompt_embeddings.to(self.visual_prompt_query_proj.weight.dtype)
+            visual_prompt_embeddings = self.visual_prompt_query_proj(visual_prompt_embeddings)
+            visual_prompt_embeddings = self.visual_prompt_query_model(visual_prompt_embeddings)  # (B, C)
+            if self.add_cross_attn_layer:
+                clip_feat = self.last_clip_feature[batch_idxs].permute(1, 0, 2)  # (B, HW, C)
+                visual_prompt_embeddings = self.visual_prompt_query_cross_attn(
+                    visual_prompt_embeddings.unsqueeze(0), clip_feat,
+                )[0, :]
+                visual_prompt_embeddings = self.visual_prompt_query_ffn(visual_prompt_embeddings)
+        else:
+            visual_prompt_embeddings = visual_prompt_embeddings.to(self.query_proj.weight.dtype)
+            visual_prompt_embeddings = self.query_proj(visual_prompt_embeddings)
+            visual_prompt_embeddings = self.model(visual_prompt_embeddings)  # (B, C)
+            if self.add_cross_attn_layer:
+                clip_feat = self.last_clip_feature[batch_idxs].permute(1, 0, 2)  # (B, HW, C)
+                visual_prompt_embeddings = self.query_cross_attn(
+                    visual_prompt_embeddings.unsqueeze(0), clip_feat,
+                )[0, :]
+                visual_prompt_embeddings = self.query_ffn(visual_prompt_embeddings)
+        return visual_prompt_embeddings
+
+    def init_visual_prompt_weights(self):
+        if self.visual_prompt_query_proj is not None:
+            self.visual_prompt_query_proj.load_state_dict(self.query_proj.state_dict())
+        if self.visual_prompt_query_model is not None:
+            self.visual_prompt_query_model.load_state_dict(self.model.state_dict())
+        if self.visual_prompt_query_cross_attn is not None:
+            self.visual_prompt_query_cross_attn.load_state_dict(
+                self.query_cross_attn.state_dict()
+            )
+        if self.visual_prompt_query_ffn is not None:
+            self.visual_prompt_query_ffn.load_state_dict(
+                self.query_ffn.state_dict()
+            )
+        return
 
 class ProjectorModel_OMG_LLaVA(PreTrainedModel):
     _auto_class = 'AutoModel'
